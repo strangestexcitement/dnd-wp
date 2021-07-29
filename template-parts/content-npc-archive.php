164c9e3741eed74cc796ea5ad7ef0ad0ab0f433e
Rachel Schnaubelt <rachel.schnaubelt@gmail.com>
2021-07-28 21:37:47 -0400

    start formatting/tweaking npc archive page

--- template-parts/content-npc-archive.php
+++ template-parts/content-npc-archive.php
@@ -10,7 +10,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
 		<?php 
 
 			$npc_link = get_permalink($post->ID);
@@ -21,7 +20,9 @@
 			$npc_class = setField(get_field('npc_class'));
 			$npc_level = setField(get_field('npc_level'));
 			$npc_race = setField(get_field('npc_race'));
-			$npc_alignment = setField(get_field('npc_alignment'));
+
+			$has_desc = ($npc_class || $npc_level || $npc_occupation || $npc_race);
+
 
 			$npc_image_id = get_field('npc_image');
 			if($npc_image_id) {
@@ -30,16 +31,23 @@
 			}
 		?>
 
-	</header><!-- .entry-header -->
-
 	<?php dndest_post_thumbnail(); ?>
 
 	<a href="<?= $npc_link ?>">
 		<div class="npc__card">
 			
-			<?= ($npc_name) ? "<p class='entry-title npc__name'>$npc_name</p>" : "" ?>
+			<?= ($npc_name) ? "<h2 class='entry-title npc__name'>$npc_name</h2>" : "" ?>
 
 			<?= ($npc_image_id) ? $npc_image : "" ?>
+
+			<?= ($has_desc) ? "<div class='npc__desc'><p>" : "" ?>
+				<?= ($npc_level) ? "Level $npc_level" : "" ?>
+				<?= ($npc_race) ? " $npc_race<br>" : "" ?>
+				<?= ($npc_class) ? " $npc_class" : "" ?>
+			<?= ($has_desc) ? "</p>" : "" ?>
+				<?= ($npc_occupation) ? "<p>$npc_occupation</p>" : "" ?>
+			<?= ($has_desc) ? "</div>" : "" ?>
+
 
 
 		</div>
